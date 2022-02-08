0ec0070d5f7be7889b3130f5636312bbab9a4b5a
Mostafa Ahangarha <ahangarha@riseup.net>
2022-02-09 02:52:07 +0300

    show humar readable date difference

--- src/index.js
+++ src/index.js
@@ -18,6 +18,22 @@ menuItems.forEach((item) => {
   item.addEventListener('click', (event) => toggleMenu(event, false));
 })
 
+function dateDiff(dateStr) {
+  const now = Date.now();
+  const date = new Date(dateStr);
+
+  const diffDays = Math.floor((now - date) / (1000 * 60 * 60 * 24));
+  console.log(now);
+
+  if (diffDays >= 60) {
+    return `${Math.floor(diffDays / 30)} months ago`;
+  } else if (diffDays >= 30) {
+    return `${Math.floor(diffDays / 30)} month ago`;
+  } else {
+    return `${diffDays} days ago`;
+  }
+}
+
 // inject projects
 const projectContainter = document.getElementById('projectContainter');
 
@@ -76,7 +92,7 @@ projects.forEach((project) => {
               d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
             ></path>
           </svg>
-          <span>2 month ago</span>
+          <span>${dateDiff(project.publishedOn)}</span>
         </div>
       </div>
     </div>
