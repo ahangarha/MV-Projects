9d17df4f1660f6c835bfc0d7d17d574e44420480
Amar Hamlaoui <amarhamlaoui@gmail.com>
2022-02-08 16:42:27 +0100

    Add project information to data.js

--- src/data.js
+++ src/data.js
@@ -9,4 +9,14 @@ export default [
     screenshot: 'https://github.com/ahangarha/SFD2022-sample-design/raw/main/screenshot.png',
     publishedOn: '2022-02-03',
   },
+  {
+    title: 'Capstone - Module 1',
+    description: 'Creative Anime Convetion 2022 is the gathering of everything anime and otaku culture, celebrating the many aspects of Japanese pop culture and its influence on the world,taking place in Algeria.',
+    author: 'Ammar Hamlaoui',
+    ghUsername: 'mirouhml',
+    repo: 'https://github.com/mirouhml/anime-convention-project',
+    license: 'MIT',
+    screenshot: 'https://github.com/ahangarha/SFD2022-sample-design/raw/main/screenshot.png',
+    publishedOn: '2022-02-01',
+  }
 ];
