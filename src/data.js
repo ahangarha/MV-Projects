e8a72a888cb63e969b85dbfae48b16732f9e0c5c
Mostafa Ahangarha <ahangarha@riseup.net>
2022-02-20 20:55:51 +0300

    add techs to projects

--- src/data.js
+++ src/data.js
@@ -6,17 +6,22 @@ export default [
     ghUsername: 'ahangarha',
     repo: 'https://github.com/ahangarha/sfd2022-sample-design/',
     license: 'AGPL',
-    screenshot: 'https://github.com/ahangarha/SFD2022-sample-design/raw/main/screenshot.png',
+    screenshot:
+      'https://github.com/ahangarha/SFD2022-sample-design/raw/main/screenshot.png',
     publishedOn: '2022-02-03',
+    techs: ['HTML/CSS'],
   },
   {
     title: 'Module 1 capstone project - Creative Anime Convention website',
-    description: 'This website is for an anime convention it has two pages a home page and about page, the home page contains information about the convention (events, guests and sponsors) and the about page contains information about the ones holding the anime convention.',
+    description:
+      'This website is for an anime convention it has two pages a home page and about page, the home page contains information about the convention (events, guests and sponsors) and the about page contains information about the ones holding the anime convention.',
     author: 'Ammar Hamlaoui',
     ghUsername: 'mirouhml',
     repo: 'https://github.com/mirouhml/anime-convention-project',
     license: 'MIT',
-    screenshot: 'https://github.com/mirouhml/anime-convention-project/raw/main/src/screenshot.png',
+    screenshot:
+      'https://github.com/mirouhml/anime-convention-project/raw/main/src/screenshot.png',
     publishedOn: '2022-02-01',
+    techs: ['HTML/CSS'],
   },
 ];
